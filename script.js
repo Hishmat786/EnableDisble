e860f58ef3888d42e4894d365c4dcbaa1bf02810
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-03-24 15:06:32 +0500

    disable and enable button added

---
+++ script.js
@@ -0,0 +1,22 @@
+const enableRadio = document.getElementById('enable');
+  const disableRadio = document.getElementById('disable');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  enableRadio.addEventListener('change', function() {
+    if (this.checked) {
+      checkboxes.forEach(checkbox => {
+        checkbox.disabled = false;
+        checkbox.parentElement.classList.remove('disabled');
+      });
+    }
+  });
+
+  disableRadio.addEventListener('change', function() {
+    if (this.checked) {
+      checkboxes.forEach(checkbox => {
+        checkbox.checked = false; // Uncheck the checkbox before disabling it
+        checkbox.disabled = true;
+        checkbox.parentElement.classList.add('disabled');
+      });
+    }
+  });
